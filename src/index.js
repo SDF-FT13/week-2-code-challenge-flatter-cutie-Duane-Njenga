d74dc0ed147245974f420926a330d4769e040dc7
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-22 10:54:58 +0300

    Added Span

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+document.addEventListener('DOMContentLoaded', () => {
+    let characterBar = document.getElementById('character-bar');
+
+    fetch(`http://localhost:3000/characters`)
+    .then(res => res.json())
+    .then(characters => {characters.forEach(character => {
+        const span = document.createElement('span');
+        span.textContent = character.name;
+
+        characterBar.appendChild(span)
+
+     })
+    });
+
+})
