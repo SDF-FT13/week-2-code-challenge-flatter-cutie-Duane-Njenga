acada7c8de6450b04786ba0956ab234f95e596e0
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-22 12:03:16 +0300

    Added Character Display

--- src/index.js
+++ src/index.js
@@ -10,8 +10,33 @@ document.addEventListener('DOMContentLoaded', () => {
         span.textContent = character.name;
 
         characterBar.appendChild(span)
+        span.addEventListener('click', () => {
+            displayCharacterDetails(character.id)
 
-     })
-    });
+            })
+         });
 
+    })
 })
+
+function displayCharacterDetails(id){
+    fetch(`http://localhost:3000/characters/${id}`)
+    .then(res => res.json())
+    .then(character => {
+       let name = document.getElementById('name');
+       let  image = document.querySelector('#detailed-info img');
+       let votes = document.getElementById('vote-count')
+        
+       name.textContent = `${character.name}`;
+        image.src = `${character.image}`;
+        image.alt = `${character.name}`;
+        votes.textContent = `${character.votes}`;
+
+    });
+}
+
+
+
+
+
+
