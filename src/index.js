f0977e510edd7ea0c53da4fda75151b6ead720ed
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-23 09:41:14 +0300

    Added Bonus Deliverables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // Your code here
-
+let characterBar = document.getElementById('character-bar');
 document.addEventListener('DOMContentLoaded', () => {
-    let characterBar = document.getElementById('character-bar');
-    let characterId = null;
+
 
     fetch(`http://localhost:3000/characters`)
     .then(res => res.json())
@@ -21,38 +20,39 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+let characterVotes = document.getElementById('vote-count')
+
 function displayCharacterDetails(id){
     fetch(`http://localhost:3000/characters/${id}`)
     .then(res => res.json())
     .then(character => {
-       let name = document.getElementById('name');
-       let  image = document.querySelector('#detailed-info img');
-       let votes = document.getElementById('vote-count')
 
+        let name = document.getElementById('name');
+        let  image = document.querySelector('#detailed-info img');
 
        name.textContent = `${character.name}`;
         image.src = `${character.image}`;
         image.alt = `${character.name}`;
-        votes.textContent = `${character.votes}`;
+        characterVotes.textContent = `${character.votes}`;
 
     });
 }
 
 
+
+
+//Addition of votes
 let form = document.getElementById('votes-form');
 
-
-
-//Addition of votes 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let characterVotes = document.getElementById('vote-count')
+
     let newVotes = parseInt(document.getElementById('votes').value);
     let voteCount = parseInt(document.getElementById('vote-count').textContent);
 
     if(!isNaN(newVotes) && !isNaN(voteCount)){
-        let votes = voteCount + newVotes
-        characterVotes.innerText = `${votes}`
+        let updatedVotes = voteCount + newVotes
+        characterVotes.innerText = `${updatedVotes}`
     } else{
         alert('Input valid Number');
         
@@ -60,16 +60,64 @@ form.addEventListener('submit', (e) => {
         
     form.reset();
 
-})
+});
 
 
 //Reset functionality 
-let resetBtn = document.getElementById('reset-btn')
+let resetBtn = document.getElementById('reset-btn');
 
 resetBtn.addEventListener('click', () => {
     let characterVotes = document.getElementById('vote-count')
     characterVotes.innerText = '0'
+});
+
+//Adding characters
+
+let characterForm = document.getElementById('character-form');
+
+characterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(characterForm);
+
+    let newCharacterName = document.querySelector('#character-form #name').value
+    console.log(newCharacterName);
+    
+    const span = document.createElement('span');
+    span.textContent = newCharacterName;
     
     
+    characterBar.appendChild(span)
+    characterForm.reset();
+
+
+    let name = document.getElementById('name')
+    let  image = document.querySelector('#detailed-info img');
+    let imageInput = document.getElementById('image-url').value
+    function immediateDisplay(){  
+        name.textContent = `${newCharacterName}`;
+        image.src = `${imageInput}`;
+        image.alt = `${newCharacterName}`;
+        characterVotes.textContent = ` 0`;
+    
+    }
+
+    immediateDisplay()
+
+
+    span.addEventListener('click', () => {
+        let name = document.getElementById('name');
+        let  image = document.querySelector('#detailed-info img');
+        let imageInput = document.getElementById('image-url').value
+
+
+        name.textContent = `${newCharacterName}`;
+        image.src = `${imageInput}`;
+        image.alt = `${newCharacterName}`;
+        characterVotes.textContent = ` 0`;
+
+
+    })
 })
+
+
 
