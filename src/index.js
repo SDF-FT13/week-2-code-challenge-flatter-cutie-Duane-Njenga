b1030820fc538dc43d04f4a6615eaaa4f6679718
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-23 08:38:31 +0300

    Added Vote count

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let characterBar = document.getElementById('character-bar');
+    let characterId = null;
 
     fetch(`http://localhost:3000/characters`)
     .then(res => res.json())
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+
 function displayCharacterDetails(id){
     fetch(`http://localhost:3000/characters/${id}`)
     .then(res => res.json())
@@ -26,7 +28,8 @@ function displayCharacterDetails(id){
        let name = document.getElementById('name');
        let  image = document.querySelector('#detailed-info img');
        let votes = document.getElementById('vote-count')
-        
+
+
        name.textContent = `${character.name}`;
         image.src = `${character.image}`;
         image.alt = `${character.name}`;
@@ -35,6 +38,29 @@ function displayCharacterDetails(id){
     });
 }
 
+
+let form = document.getElementById('votes-form');
+
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let characterVotes = document.getElementById('vote-count')
+    let newVotes = parseInt(document.getElementById('votes').value);
+    let voteCount = parseInt(document.getElementById('vote-count').textContent);
+
+    if(!isNaN(newVotes) && !isNaN(voteCount)){
+        let votes = voteCount + newVotes
+        characterVotes.innerText = `${votes}`
+    } else{
+        alert('Input valid Number');
+        
+    }
+        
+    form.reset();
+
+})
 
 
 
